b93745e0743508c2c08a0546e332770f997cb23d
kingchobo2022 <120370460+kingchobo2022@users.noreply.github.com>
2026-09-19 11:22:41 +0900

    Add PostController with CRUD methods for posts

---
+++ 37_PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(PostRequest $request)
+    {
+        $validated = $request->validated();
+
+        $post = Post::create([
+            'title' => $validated['title'],
+            'content' => $validated['content'],
+            'author' => '홍길동',
+            'is_published' => true,
+        ]);
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', '새 게시글이 성공적으로 등록되었습니다.');
+
+    }
+
+    public function search(Request $request)
+    {
+        $keyword = $request->input('keyword', '');
+
+        return view('posts.search', compact('keyword'));
+    }
+
+    public function index(Request $request)
+    {
+        // $posts = Post::all();
+        //$query = Post::where('is_published', true)->latest();
+        $query = Post::where('is_published', true)
+                ->withCount('comments')
+                ->latest();
+
+        if ($request->filled('keyword')) {
+            $keyword = $request->input('keyword');
+            $query->where('title', 'like', "%{$keyword}%");
+        }
+
+        //$posts = $query->get();
+        $posts = $query->paginate(10)->withQueryString();
+
+        return view('posts.index', compact('posts'));
+
+    }
+
+    public function show(int $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('posts.show1', compact('post'));
+    }
+
+    public function edit(int $id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(PostRequest $request, int $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $post->update($request->validated());
+
+        return redirect()
+            ->route('posts.show', $post->id)
+            ->with('success', '게시글이 성공적으로 수정되었습니다.');
+    } 
+
+    public function destroy(int $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', '게시글이 성공적으로 삭제되었습니다.');
+    }
+
+
+}
