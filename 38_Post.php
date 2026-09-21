a491fdb3accd190fb0f4ae95d86e2f6f2ea94333
kingchobo2022 <120370460+kingchobo2022@users.noreply.github.com>
2026-09-22 07:52:24 +0900

    Create Post model with relationships and fillable fields

---
+++ 38_Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'views',
+        'is_published',
+        'published_at',
+        'thumbnail',
+    ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
